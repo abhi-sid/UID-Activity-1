02697cc8a41671e265757e16b6cab9b8f2bf114e
Abhishek Ghosh <96966928+abhi-sid@users.noreply.github.com>
2022-11-27 14:26:13 +0530

    Add files via upload

---
+++ Activity-1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        .box{
+        max-width: 500px;
+        margin: auto;
+        width:400px;
+        background-color: palegreen;
+        display:inline-block; 
+        }
+    </style>
+</head>
+<body>
+<center>
+    <h3>Abhishek Siddhartha Ghosh 1CR19CS222</h3>
+    <br>
+    <h5>UID Activity-1</h5>
+    <br>
+    </center>
+<form method="get">
+	<div class="box">
+        <fieldset>
+            <legend><b>Personnel</b></legend>
+            <br/><br/>
+			<label>Manager: </label>
+			<input type="text" name="Mngr" size="25"/><br/><br>
+			<label>Employees: </label>
+			<input type="text" name="empls" size="10"/><br/><br>
+			<label>Payroll: </label>
+			<input type="text" name="payrl" size="17"/><br/><br>
+		</fieldset>
+	</div>
+</form>
+    </body>
+</html>
